6f914e487ec21009306a11221f5567713af38516
Umar <muhammad.umar@codilidtysolutions.com>
2021-11-05 14:54:24 +0500

    ImageSliderComplete

--- src/screens/SelectProduct/SelectProduct.js
+++ src/screens/SelectProduct/SelectProduct.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, ScrollView, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import { onChange } from 'react-native-reanimated';
+import { SliderBox } from "react-native-image-slider-box";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import styles from './Styles';
 const SelectProduct = ({ navigation }) => {
@@ -8,70 +10,29 @@ const SelectProduct = ({ navigation }) => {
     const width = wp('100%');
     const height = hp('40%');
 
-    const [active, setActive] = useState(0);
     const images = [
-        'https://images.pexels.com/photos/4346353/pexels-photo-4346353.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        'https://images.pexels.com/photos/6077783/pexels-photo-6077783.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        'https://images.pexels.com/photos/5117800/pexels-photo-5117800.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-        'https://images.pexels.com/photos/4131522/pexels-photo-4131522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        'https://images.pexels.com/photos/6102477/pexels-photo-6102477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        // require('../../assets/images/prodcut1.png'),
-    ];
+        require('../../assets/images/bgforslider1.jpg'),
+        require('../../assets/images/prodcut1.png'),
+        require('../../assets/images/prodcut3.png'),
 
-    const onChange = ({ layoutMeasurement, contentOffset, contentSize}) => {
-        const paddingToBottom = 20;
-        const slide = layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
-        if (slide !== active) {
-          setActive(slide);
-        }
-      };
-      const change = ({event}) => {
-        const slide = Math.ceil(
-          event.nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
-        );
-        if (slide !== active) {
-          setActive(slide);
-        }
-      };
-    //   const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-    //     const paddingToBottom = 20;
-    //     const slide = layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
-    //     if (slide !== active) {
-    //         setActive(slide);
-    //       }
-    //     // return slide
-    //   };
+    ];
     return (
         <SafeAreaView style={styles.container}>
-            <View style ={styles.wrap}>
-                <ScrollView
-                onScroll={({nativeEvent}) => {
-                    onChange(nativeEvent)
-                  }}
-                pagingEnabled
-                horizontal
-                scrollEventThrottle={24}
-                showsHorizontalScrollIndicator={false}
-                style={styles.wrap}>
-                    {
-                        images.map((e, index) =>
-                        <Image 
-                        key = {e}
-                        resizeMode='stretch'
-                        style={styles.wrap}
-                        source = {{uri: e}}
-                        /> 
-                        )
-                    }
-                </ScrollView>
-                <View style={styles.wrapDot}>
-                    {images.map((i, k) => (
-                    <Text key={k} style={k === active ? styles.activeDot : styles.dot}>
-                        •
-                    </Text>
-                    ))}
-                </View>
-            </View>
+              <SliderBox
+                ImageComponentStyle= {{resizeMode: 'cover', overFLow: 'hidden'}}
+                autoplay={true}
+                circleLoop={true}
+                images={images}
+                resizeMethod={'resize'}
+                resizeMode={'cover'}
+                sliderBoxHeight={250}
+                inactiveDotColor= "white"
+                dotColor="#7054D5"
+                dotStyle = {{padding: 0}}
+              />
+              <TouchableOpacity style={styles.backBtn} onPress={() => navigation.navigate('HomeProduct')}>
+                <FontAwesome name={"long-arrow-left"} size={20} color={"white"} />
+              </TouchableOpacity>
         </SafeAreaView>
     )
 }
--- src/screens/SelectProduct/Styles.js
+++ src/screens/SelectProduct/Styles.js
@@ -9,23 +9,11 @@ const styles = StyleSheet.create({
     flex: 1, 
     backgroundColor: 'white'
   },
-  wrap: {
-    width: WIDTH,
-    height: HEIGHT * 0.35,
-  },
-  wrapDot: {
-    flexDirection: 'row',
-    position: 'absolute',
-    alignSelf: 'center',
-    bottom: 0,
-  },
-  dot: {
-    color: 'white',
-    fontSize: hp('5%'),
-  },
-  activeDot: {
-    color: '#7E4FD0',
-    fontSize: hp('5%'),
+  backBtn: { 
+    position: 'absolute', 
+    padding: '2%', 
+    marginLeft: wp('5%'), 
+    marginTop: hp('2%'),
   },
 });
 
