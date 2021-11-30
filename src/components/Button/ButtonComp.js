864efdfd82798bc1f4ad36ba722bed2fccaa8e26
Umar <muhammad.umar@codilidtysolutions.com>
2021-11-30 10:00:25 +0500

    MyOrderFilter

--- src/components/Button/ButtonComp.js
+++ src/components/Button/ButtonComp.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { TouchableOpacity, Text } from "react-native";
 import styles from './styles';
 
-const ButtonComp = (props) => {
+const ButtonComp = (props) => {  
 
   return (
     <TouchableOpacity style={[styles.buttonLogin, props.styleBtn]} onPress = {props.onPress}>
